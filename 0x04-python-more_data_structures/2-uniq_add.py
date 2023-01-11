0aa4b658de6c7843c46da9a2de949197a4280683
Herrings <mkwara.h@gmail.com>
2023-01-11 02:21:05 -0800

    Task 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-def complex_delete(a_dictionary, value):
-    list_keys = list(a_dictionary.keys())
-
-    for value_dic in list_keys:
-        if value == a_dictionary.get(value_dic):
-            del a_dictionary[value_dic]
-
-    return (a_dictionary)
+def uniq_add(my_list=[]):
+    add = 0
+    for i in set(my_list):
+        add += i
+    return add
