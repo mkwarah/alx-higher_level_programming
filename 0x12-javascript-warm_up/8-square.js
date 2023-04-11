e19438f0384682b178c5eb3a30646ddc5d851ace
Herrings <mkwara.h@gmail.com>
2023-04-11 05:40:38 -0700

    Task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-const { argv } = require('process');
-const size = Number(argv[2]);
-const repeat = 'X'.repeat(size);
+const size = Math.floor(Number(process.argv[2]));
 if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < size; i++) {
-    console.log(repeat);
+  for (let r = 0; r < size; r++) {
+    let row = '';
+    for (let c = 0; c < size; c++) row += 'X';
+    console.log(row);
   }
 }
-
